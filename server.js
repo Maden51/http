7e044741dc852f4603868cd97e3993ca8e58ac7a
Maden51 <madenon123@gmail.com>
2021-12-21 13:52:55 +0300

    1.0.6

---
+++ server.js
@@ -0,0 +1,52 @@
+import http from 'http';
+import Koa from 'koa';
+import koaBody from 'koa-body';
+import cors from 'koa2-cors';
+import Router from 'koa-router';
+import Tickets from './src/js/API/Tickets';
+
+const app = new Koa();
+const tickets = new Tickets();
+const router = new Router();
+
+app.use(cors());
+app.use(koaBody({
+  urlencoded: true,
+  text: true,
+  json: true,
+}));
+
+router.get('/allTickets', async (ctx, next) => {
+  ctx.response.body = tickets.storage.map((item) => ({
+    id: item.id,
+    status: item.status,
+    name: item.name,
+    created: item.created,
+  }));
+});
+
+router.get('/ticketById', async (ctx, next) => {
+  const ticketId = Number(ctx.request.query.id);
+  ctx.response.body = tickets.getTicketById(ticketId);
+});
+
+router.delete('/deleteTicketById', async (ctx, next) => {
+  const ticketId = Number(ctx.request.query.id);
+  ctx.response.body = tickets.deleteTicket(ticketId);
+});
+
+router.post('/createTicket', async (ctx, next) => {
+  const { name, description } = ctx.request.query;
+  ctx.response.body = tickets.createTicket(name, description);
+});
+
+router.post('/changeTicketStatus', async (ctx, next) => {
+  const ticketId = Number(ctx.request.query.id);
+  ctx.response.body = tickets.changeTicketStatus(ticketId);
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = process.env.PORT || 8080;
+const server = http.createServer(app.callback());
+server.listen(port, () => console.log('server started'));
